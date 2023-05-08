ac80d56ca72f01043da9344638692b8b59cfac8a
brandon <ruipengyu.li@gmail.com>
2023-05-08 11:05:28 +0100

    finish ex5

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,14 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    if (head == NULL) return 0;
+    node *tortois = head;
+    node *hare = head;
+    while (hare->next != NULL 
+            && hare->next->next != NULL) {
+        hare = hare->next->next;
+        tortois = tortois->next;
+        if (hare == tortois) return 1;
+    }
     return 0;
 }
